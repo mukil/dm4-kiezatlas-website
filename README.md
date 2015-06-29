9d670e99f8a1c9486363dc2dbf559d8c7cda549f
Malte Reißig <malte@mikromedia.de>
2015-06-29 10:06:37 +0200

    Initial commit: Basic geo-spatial query working.

---
+++ README.md
@@ -0,0 +1,21 @@
+
+Kiezatlas Website
+#################
+
+This DeepaMehta 4 Plugin is the software module for ship- and development of the rewritten website to be hosted at www.kiezatlas.de. 
+
+It builds on our work of last year, especially on the 
+
+ * [dm4-kiezatlas](http://github.com/jri/dm4-kiezatlas) module
+ * [dm4-geospatial](http://github.com/jri/dm4-geospatial) module
+
+but also on other open source software, such as
+
+ * [jQuery](http://www.jquery.com)
+ * [LeafletJS](http://www.leafletjs.com)
+ * [jQuery Mobile](http://www.jquerymobile.com)
+
+and on the Tile Map Service of [www.mapbox.com](http://www.mapbox.com).
+
+Author:
+Malte Reißig, Copyright 2015
---
+++ src/main/java/de/kiezatlas/website/KiezatlasWebsitePlugin.java
@@ -0,0 +1,82 @@
+package de.kiezatlas.website;
+
+import de.deepamehta.core.RelatedTopic;
+import de.deepamehta.core.Topic;
+import java.util.logging.Logger;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+
+
+import de.deepamehta.core.osgi.PluginActivator;
+import de.deepamehta.core.service.Inject;
+import de.deepamehta.core.service.ResultList;
+import de.deepamehta.plugins.geomaps.model.GeoCoordinate;
+import de.deepamehta.plugins.geomaps.service.GeomapsService;
+import de.deepamehta.plugins.geospatial.service.GeospatialService;
+import de.kiezatlas.service.KiezatlasService;
+import de.kiezatlas.website.model.KiezatlasEntry;
+import java.util.ArrayList;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ * @author Malte Reißig (<malte@mikromedia.de>)
+ * @website http://github.com/mukil/dm4-kiezatlas-website
+ * @version 0.1-SNAPSHOT - compatible with DeepaMehta 4.4
+ *
+ */
+
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class KiezatlasWebsitePlugin extends PluginActivator {
+
+    private Logger log = Logger.getLogger(getClass().getName());
+
+    @Inject
+    KiezatlasService wsService;
+
+    @Inject
+    GeospatialService geospatialService;
+
+    @Inject
+    GeomapsService geomapsService;
+    
+    @GET
+    @Path("/map/{webAlias}")
+    public List<RelatedTopic> getCityMap(@PathParam("webAlias") String webAlias) {
+        // ### Do fetch generic "Kiezatlas Site" with focus on the region represented in 
+        //     the given "City Map" Web Alias
+        return null;
+    }
+    
+    @GET
+    @Path("/search/{coordinatePair}/{radius}")
+    public List<KiezatlasEntry> getGeoObjectsNearBy(@PathParam("coordinatePair") String coordinates, 
+            @PathParam("radius") String radius) {
+        double lon = 13.4, lat = 52.5;
+        double r;
+        r = (radius.isEmpty() || radius.equals("0")) ? 1.0 : Double.parseDouble(radius);
+        List<Topic> geoCoordTopics = geospatialService.getTopicsWithinDistance(new GeoCoordinate(lon, lat), r);
+        ArrayList<KiezatlasEntry> results = new ArrayList<KiezatlasEntry>();
+        for (Topic geoCoordTopic : geoCoordTopics) {
+            GeoCoordinate geoCoord = geomapsService.geoCoordinate(geoCoordTopic);
+            Topic address = geoCoordTopic.getRelatedTopic("dm4.core.composition", "dm4.core.child", "dm4.core.parent", "dm4.contacts.address");
+            if (address != null) {
+                ResultList<RelatedTopic> geoObjects = address.getRelatedTopics("dm4.core.composition", "dm4.core.child", "dm4.core.parent", "ka2.geo_object", 0);
+                for (RelatedTopic geoObject : geoObjects) {
+                    results.add(new KiezatlasEntry(geoObject, geoCoordTopic, geoCoord));
+                }
+            } else {
+                log.info("No Address Entry found for geo-coordinate " + geoCoordTopic.getSimpleValue());
+            }
+        }
+        return results;
+    }
+
+}
