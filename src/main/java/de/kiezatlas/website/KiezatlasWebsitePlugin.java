4882a52c9fa569821ee7fee39a4727fdb7ee9a35
Malte Reißig <malte@mikromedia.de>
2015-07-06 15:50:44 +0200

    A couple of basic improvements towards our new website.
    - Adapted view-model for website about bezirksregionen (and uri)
      in preparation for linking  into all our existing citymaps.
    - Added experimental full-text search results map display
    - Added geo-coding service to allow text-based location input
    - Basic detail window is working
      (no linking into citymaps yet)
    
    TODOS:
    - render geo_objects (from many queries) just once on map
    - deal with multiple geo_objects on same location (via geo_coordinate_id)
    - intrdouce name-display on hover
    - visually highlight selected elements in map
    - automate location query/attach editable circle to current location input.

--- src/main/java/de/kiezatlas/website/KiezatlasWebsitePlugin.java
+++ src/main/java/de/kiezatlas/website/KiezatlasWebsitePlugin.java
@@ -16,11 +16,23 @@ import de.deepamehta.core.Topic;
 import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.Inject;
 import de.deepamehta.core.service.ResultList;
+import de.deepamehta.core.service.Transactional;
 import de.deepamehta.plugins.geomaps.model.GeoCoordinate;
 import de.deepamehta.plugins.geomaps.service.GeomapsService;
 import de.deepamehta.plugins.geospatial.service.GeospatialService;
 import de.kiezatlas.service.KiezatlasService;
-import de.kiezatlas.website.model.KiezatlasEntry;
+import de.kiezatlas.website.model.EntryView;
+import de.kiezatlas.website.model.MapEntryView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import javax.ws.rs.QueryParam;
 
 
 
@@ -31,7 +43,7 @@ import de.kiezatlas.website.model.KiezatlasEntry;
  *
  */
 
-@Path("/")
+@Path("/kiezatlas/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class KiezatlasWebsitePlugin extends PluginActivator {
@@ -46,10 +58,16 @@ public class KiezatlasWebsitePlugin extends PluginActivator {
 
     @Inject
     GeomapsService geomapsService;
+    
+    @GET
+    @Path("/topic/{topicId}")
+    public EntryView getKiezatlasTopicView(@PathParam("topicId") long topicId) {
+        return new EntryView(dms.getTopic(topicId), geomapsService);
+    }
 
     @GET
     @Path("/search/{coordinatePair}/{radius}")
-    public List<KiezatlasEntry> getGeoObjectsNearBy(@PathParam("coordinatePair") String coordinates, 
+    public List<MapEntryView> getGeoObjectsNearBy(@PathParam("coordinatePair") String coordinates, 
             @PathParam("radius") String radius) {
         double lon = 13.4, lat = 52.5;
         if (coordinates != null && !coordinates.isEmpty() && coordinates.contains(",")) {
@@ -59,20 +77,79 @@ public class KiezatlasWebsitePlugin extends PluginActivator {
         double r;
         r = (radius.isEmpty() || radius.equals("0")) ? 1.0 : Double.parseDouble(radius);
         List<Topic> geoCoordTopics = geospatialService.getTopicsWithinDistance(new GeoCoordinate(lon, lat), r);
-        ArrayList<KiezatlasEntry> results = new ArrayList<KiezatlasEntry>();
+        ArrayList<MapEntryView> results = new ArrayList<MapEntryView>();
         for (Topic geoCoordTopic : geoCoordTopics) {
             GeoCoordinate geoCoord = geomapsService.geoCoordinate(geoCoordTopic);
             Topic address = geoCoordTopic.getRelatedTopic("dm4.core.composition", "dm4.core.child", "dm4.core.parent", "dm4.contacts.address");
             if (address != null) {
                 ResultList<RelatedTopic> geoObjects = address.getRelatedTopics("dm4.core.composition", "dm4.core.child", "dm4.core.parent", "ka2.geo_object", 0);
                 for (RelatedTopic geoObject : geoObjects) {
-                    results.add(new KiezatlasEntry(geoObject, geoCoordTopic, geoCoord));
+                    results.add(new MapEntryView(geoObject, geoCoordTopic, geoCoord));
                 }
             } else {
                 log.info("No Address Entry found for geo-coordinate " + geoCoordTopic.getSimpleValue());
             }
         }
         return results;
+    }
+    
+    @GET
+    @Path("/search")
+    @Transactional
+    public List<MapEntryView> searchTopics(@QueryParam("search") String query) {
+        try {
+            log.info("> query=\"" + query + "\"");
+            ArrayList<MapEntryView> results = new ArrayList<MapEntryView>();
+            if (query.isEmpty()) {
+                log.warning("No search term entered, returning empty resultset");
+                return results;
+            }
+            List<Topic> singleTopics = dms.searchTopics(query, null);
+            log.info(singleTopics.size() + " topics found");
+            for (Topic topic : singleTopics) {
+                if (topic.getTypeUri().equals("ka2.geo_object.name")) {
+                    Topic geoObject = topic.getRelatedTopic("dm4.core.composition", "dm4.core.child", "dm4.core.parent", "ka2.geo_object");
+                    results.add(new MapEntryView(geoObject, geomapsService));
+                } else {
+                    log.info("### NO-MATCH: Found " + topic.getTypeUri() + " data-entry");
+                }
+            }
+            return results;
+        } catch (Exception e) {
+            throw new RuntimeException("Searching topics failed", e);
+        }
+    }
+
+    @GET
+    @Path("/geocode/{input}")
+    public String geoCodeAddressInput(@PathParam("input") String input) {
+        String query = input;
+        String result = "";
+        query = query.substring(2, query.length() - 1);
+        try {
+            // Encoded url to open
+            query = URLEncoder.encode(query, "UTF-8");
+            String url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + query + "&sensor=false&locale=de";
+            URLConnection connection = new URL(url).openConnection();
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Charset", "UTF-8");
+            // Get the response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            result = sb.toString();
+        } catch (UnsupportedEncodingException ex) {
+            log.log(Level.WARNING, "Unsuporrted Encoding Exception", ex);
+        } catch (MalformedURLException mux) {
+            log.log(Level.WARNING, "Malformed URL Exception", mux);
+        } catch (IOException ioex) {
+            log.log(Level.WARNING, "IOException", ioex);
+        }
+        return result;
     }
 
 }
---
+++ src/main/java/de/kiezatlas/website/model/EntryView.java
@@ -0,0 +1,97 @@
+package de.kiezatlas.website.model;
+
+import de.deepamehta.core.JSONEnabled;
+import de.deepamehta.core.RelatedTopic;
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.service.ResultList;
+import de.deepamehta.plugins.geomaps.model.GeoCoordinate;
+import de.deepamehta.plugins.geomaps.service.GeomapsService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ * 
+ * @author Malte Reißig <malte@mikromedia.de>
+ */
+public class EntryView implements JSONEnabled {
+    
+    Topic geoObject = null;
+    Topic geoCoordTopic = null;
+    GeoCoordinate geoCoordinate = null;
+    Topic bezirk = null;
+    Topic bezirksregion = null;
+    ResultList<RelatedTopic> relatedTopics = null;
+    ResultList<RelatedTopic> relatedAudiences = null;
+    ResultList<RelatedTopic> relatedServices = null;
+    
+    Logger log = Logger.getLogger(EntryView.class.getName());
+    
+     public EntryView(Topic geoObject, GeomapsService geomaps) {
+        this.geoObject = geoObject;
+        // fetch geo-coordinate via address
+        Topic addressTopic = geoObject.getRelatedTopic("dm4.core.composition", "dm4.core.parent", "dm4.core.child", "dm4.contacts.address");
+        if (addressTopic != null) {
+            this.geoCoordTopic = addressTopic.getRelatedTopic("dm4.core.composition", "dm4.core.parent", "dm4.core.child", "dm4.geomaps.geo_coordinate");
+            if (geoCoordTopic != null) {
+                this.geoCoordinate = geomaps.geoCoordinate(geoCoordTopic);      
+            } else {
+                log.log(Level.WARNING, "**** KiezatlasEntry''s Address ({0}) has no Geo Coordinate set!", addressTopic.getSimpleValue());
+            }
+        } else {
+            log.log(Level.WARNING, "**** KiezatlasEntry ({0}, {1}) has no Address set!", 
+                    new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
+        }
+        // fetch citymap-webalias (in uris)
+        this.bezirk = geoObject.getRelatedTopic("dm4.core.aggregation", "dm4.core.parent", "dm4.core.child", "ka2.bezirk");
+        this.bezirksregion = geoObject.getRelatedTopic("dm4.core.aggregation", "dm4.core.parent", "dm4.core.child", "ka2.bezirksregion"); // ### many?
+        if (bezirksregion == null) {
+            log.log(Level.WARNING, "*** KiezatlasEntry ({0}, {1}, \"{2}\") has no Bezirksregion topic set!", 
+                    new Object[]{geoObject.getUri(), geoObject.getSimpleValue(), bezirk.getSimpleValue()});
+        }
+        relatedTopics = geoObject.getRelatedTopics("dm4.core.aggregation", "dm4.core.parent", "dm4.core.child", "ka2.criteria.thema", 0);
+        relatedAudiences = geoObject.getRelatedTopics("dm4.core.aggregation", "dm4.core.parent", "dm4.core.child", "ka2.criteria.zielgruppe", 0);
+        relatedServices = geoObject.getRelatedTopics("dm4.core.aggregation", "dm4.core.parent", "dm4.core.child", "ka2.criteria.angebot", 0);
+    }
+
+    public JSONObject toJSON() {
+        try {
+            JSONObject object = new JSONObject();
+            if (geoCoordTopic == null) {
+                log.log(Level.WARNING, "KiezatlasEntry has no geo-coordinate topic, null-ing this object during serialization");
+            } else {
+                object.put("uri", geoObject.getUri())
+                    .put("id", geoObject.getId())
+                    .put("name", geoObject.getSimpleValue().toString())
+                    .put("address_name", geoObject.getChildTopics().getTopic("dm4.contacts.address").getSimpleValue())
+                    .put("geo_coordinate_lat", geoCoordinate.lat)
+                    .put("geo_coordinate_lon", geoCoordinate.lon)
+                    .put("geo_coordinate_id", geoCoordTopic.getId());
+                if (bezirk != null) {
+                    object.put("bezirk_uri", bezirk.getUri());
+                    object.put("bezirk_name", bezirk.getSimpleValue());   
+                }
+                if (bezirksregion != null) {
+                    object.put("bezirksregion_uri", bezirksregion.getUri());
+                    object.put("bezirksregion_name", bezirksregion.getSimpleValue());   
+                }
+                if (relatedTopics.getSize() > 0) {
+                    JSONArray related = new JSONArray();
+                    for (RelatedTopic relatedTopic : relatedTopics) {
+                        related.put(new JSONObject()
+                            .put("related_topic_uri", relatedTopic.getUri())
+                            .put("related_topic_name", relatedTopic.getSimpleValue().toString()));
+                    }
+                    object.put("related_topics", related);
+                }
+            }
+            return object;
+        } catch (JSONException ex) {
+            log.log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
