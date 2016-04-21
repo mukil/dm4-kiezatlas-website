def4dcec640b13a7efee153413c66001126ae66c
Malte Reißig <malte@mikromedia.de>
2016-04-21 23:07:35 +0200

    Fixes display of outdated geo object name value.

--- src/main/java/de/kiezatlas/website/model/GeoObjectView.java
+++ src/main/java/de/kiezatlas/website/model/GeoObjectView.java
@@ -38,15 +38,15 @@ public class GeoObjectView implements JSONEnabled {
     }
 
     public String getName() {
-        return this.geoObject.getSimpleValue().toString();
+        return this.geoObject.getChildTopics().getTopic("ka2.geo_object.name").getSimpleValue().toString();
     }
 
     public Topic getBezirksregion() {
         bezirksregion = geoObject.getRelatedTopic("dm4.core.aggregation", "dm4.core.parent",
-                "dm4.core.child", "ka2.bezirksregion"); // ### many?
+            "dm4.core.child", "ka2.bezirksregion"); // ### many
         if (bezirksregion == null) {
             log.log(Level.WARNING, "### Geo Object ({0}, {1}) has no BEZIRKSREGION set!",
-                    new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
+                new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
             return null;
         }
         return bezirksregion;
@@ -54,33 +54,33 @@ public class GeoObjectView implements JSONEnabled {
 
     public Topic getBezirk() {
         bezirk = geoObject.getRelatedTopic("dm4.core.aggregation", "dm4.core.parent",
-                "dm4.core.child", "ka2.bezirk");
+            "dm4.core.child", "ka2.bezirk");
         if (bezirk == null) {
             log.log(Level.WARNING, "### Geo Object ({0}, {1}) has no BEZIRK set!",
-                    new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
+                new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
             return null;
         }
         return bezirk;
     }
 
-    public GeoCoordinate getGeoCoordinate(GeomapsService geomaps) {
+    private GeoCoordinate getGeoCoordinate(GeomapsService geomaps) {
         // fetch geo-coordinate via address
         addressTopic = geoObject.getRelatedTopic("dm4.core.composition", "dm4.core.parent",
-                "dm4.core.child", "dm4.contacts.address");
+            "dm4.core.child", "dm4.contacts.address");
         if (addressTopic != null) {
             Topic geoCoordTopic = addressTopic.getRelatedTopic("dm4.core.composition", "dm4.core.parent",
-                    "dm4.core.child", "dm4.geomaps.geo_coordinate");
+                "dm4.core.child", "dm4.geomaps.geo_coordinate");
             if (geoCoordTopic != null) {
                 geoCoordTopicId = geoCoordTopic.getId();
                 addressTopic.loadChildTopics();
                 return geoCoordinate = geomaps.geoCoordinate(geoCoordTopic);
             } else {
                 log.log(Level.WARNING, "### Geo Object's Address (" + addressTopic.getId() + "," + geoObject
-                        .getSimpleValue() +") has no GEO COORDINATE set!");
+                    .getSimpleValue() + ") has no GEO COORDINATE set!");
             }
         } else {
             log.log(Level.WARNING, "### Geo Object ({0}, {1}) has no ADDRESS set!",
-                    new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
+                new Object[]{geoObject.getUri(), geoObject.getSimpleValue()});
         }
         return null;
     }
@@ -98,13 +98,17 @@ public class GeoObjectView implements JSONEnabled {
     }
 
     public String getAggregatedAddressTopicId() {
-        if (addressTopic == null) return "-1:-1";
-        return addressTopic.getChildTopics().getTopic("dm4.contacts.street").getId() + ":" +
-                addressTopic.getChildTopics().getTopic("dm4.contacts.postal_code").getId();
+        if (addressTopic == null) {
+            return "-1:-1";
+        }
+        return addressTopic.getChildTopics().getTopic("dm4.contacts.street").getId() + ":"
+            + addressTopic.getChildTopics().getTopic("dm4.contacts.postal_code").getId();
     }
 
     public String getAddressValue() {
-        if (addressTopic == null) return "";
+        if (addressTopic == null) {
+            return "";
+        }
         return addressTopic.getSimpleValue().toString();
     }
 
@@ -130,24 +134,26 @@ public class GeoObjectView implements JSONEnabled {
 
     public JSONObject toJSON() {
         // needed to display geo object
-        if (!hasGeoCoordinateValues()) return null;
+        if (!hasGeoCoordinateValues()) {
+            return null;
+        }
         // needed to link into orginal citymap
         getBezirk();
         getBezirksregion(); // has stadtplan web alias in uri
         // return valid object
         try {
             return new JSONObject()
-                    .put("uri", geoObject.getUri())
-                    .put("id", geoObject.getId())
-                    .put("name", getName())
-                    .put("address_id", getAggregatedAddressTopicId())
-                    .put("geo_coordinate_lat", getGeoCoordinateLatValue())
-                    .put("geo_coordinate_lon", getGeoCoordinateLngValue())
-                    .put("bezirk_uri", getBezirkUri())
-                    .put("bezirk_name", getBezirkName())
-                    .put("bezirksregion_uri", getBezirksregionUri())
-                    .put("bezirksregion_name", getBezirksregionName())
-                    .put("angebote_count", this.angeboteCount);
+                .put("uri", geoObject.getUri())
+                .put("id", geoObject.getId())
+                .put("name", getName())
+                .put("address_id", getAggregatedAddressTopicId())
+                .put("geo_coordinate_lat", getGeoCoordinateLatValue())
+                .put("geo_coordinate_lon", getGeoCoordinateLngValue())
+                .put("bezirk_uri", getBezirkUri())
+                .put("bezirk_name", getBezirkName())
+                .put("bezirksregion_uri", getBezirksregionUri())
+                .put("bezirksregion_name", getBezirksregionName())
+                .put("angebote_count", this.angeboteCount);
         } catch (Exception jex) {
             throw new RuntimeException("Constructing a JSON GeoObjectView FAILED", jex);
         }
