480441c4d22192b4306f5e9adbe29ea0532469eb
Malte <malte@mikromedia.de>
2018-10-24 15:09:47 +0200

    Add upcoming migration to move bild paths topics.

--- src/main/java/de/kiezatlas/website/migrations/Migration22.java
+++ src/main/java/de/kiezatlas/website/migrations/Migration22.java
@@ -1,54 +1,39 @@
 package de.kiezatlas.website.migrations;
 
 import de.deepamehta.core.Topic;
+import de.deepamehta.core.service.Inject;
 import de.deepamehta.core.service.Migration;
-import de.kiezatlas.KiezatlasService;
+import de.deepamehta.workspaces.WorkspacesService;
+import de.kiezatlas.angebote.AngebotService;
 import java.util.List;
 import java.util.logging.Logger;
 
 /**
- *
- * @author malt
+ * Remove geo objects for which users had not gathered consent.
+ * @author malted
  */
 public class Migration22 extends Migration {
 
-    private final Logger log = Logger.getLogger(getClass().getName());
+    private Logger log = Logger.getLogger(getClass().getName());
+
+    @Inject private WorkspacesService workspaces;
+    @Inject private AngebotService angebote;
 
     @Override
     public void run() {
-        List<Topic> beschreibungen = dm4.getTopicsByType("ka2.beschreibung");
-        int descrRemoved = 0, hoursRemoved = 0;
-        for (Topic beschreibung : beschreibungen) {
-            Topic geoObject = getParentGeoObjectTopic(beschreibung);
-            if (geoObject == null) {
-                log.info("Removing orphaned Beschreibungstext \"" + beschreibung.getSimpleValue() + "\"");
-                beschreibung.delete();
-                descrRemoved++;
-            }
+        log.info("### Migration 22 Started: Move Bild Path topics into Angebote workspace");
+        List<Topic> bildPaths = dm4.getTopicsByType("ka2.bild.pfad");
+        for (Topic pfad : bildPaths) {
+            log.info("Moving Bild Pfad " + pfad.getSimpleValue()  + " Topic ID: " + pfad.getId() + " to new workspace");
+            workspaces.assignToWorkspace(pfad, angebote.getAngeboteWorkspace().getId());
         }
-        List<Topic> openingHours = dm4.getTopicsByType("ka2.oeffnungszeiten");
-        for (Topic openingHour : openingHours) {
-            Topic geoObject = getParentGeoObjectTopic(openingHour);
-            if (geoObject == null) {
-                log.info("Removing orphaned Öffnungszeiten \"" + openingHour.getSimpleValue() + "\"");
-                openingHour.delete();
-                hoursRemoved++;
-            }
+        log.info("### Migration 22 Started: Move File Topics into Angebote workspace");
+        List<Topic> files = dm4.getTopicsByType("dm4.files.file");
+        for (Topic file : files) {
+            log.info("Moving File Topic " + file.getSimpleValue()  + " Topic ID: " + file.getId() + " to new workspace");
+            workspaces.assignToWorkspace(file, angebote.getAngeboteWorkspace().getId());
         }
-        log.info("Mogration 18 deleted " + descrRemoved + " orphaned Beschreibungen and " + hoursRemoved + " Öffnungszeiten topics");
-    }
-
-    private Topic getParentGeoObjectTopic(Topic entry) {
-        Topic result = null;
-        try {
-            result = entry.getRelatedTopic(null, "dm4.core.child", "dm4.core.parent", KiezatlasService.GEO_OBJECT);
-        } catch (RuntimeException ex) {
-            log.warning("Exception loading parent geo object topic cause \"" + ex.getLocalizedMessage() + "\"");
-            return result;
-        }
-        if (result == null) log.warning("Search Result Entry: " +entry.getTypeUri()
-            + ", " +entry.getId() +" has no Geo Object as PARENT"); // fulltext searches also "abandoned" facet topics
-        return result;
+        log.info("### Migration 22 COMPLETED: Move Bild Path topics into Angebote workspace");
     }
 
 }
